a2f98036f567b2a66baa474fe8441cd29f122dc5
dmitriy kalinin <dmitriy@pivotallabs.com>
2017-10-05 07:53:08 -0700

    allow recreates

--- local.sh
+++ local.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# rm -f ./creds.yml
+rm -f ./creds.yml
 
 bosh create-env docker.yml \
   --state ./state.json \
@@ -13,4 +13,5 @@ bosh create-env docker.yml \
   -v internal_ip=192.168.50.8 \
   -v internal_gw=192.168.50.1 \
   -v internal_cidr=192.168.50.1/24 \
-  -v outbound_network_name=NatNetwork
+  -v outbound_network_name=NatNetwork \
+  $@
